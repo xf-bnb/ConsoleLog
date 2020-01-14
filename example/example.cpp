1fcce5fff57e49fa13dee08212b0a74f8983f49e
FrankXiong <Frank_Xiong_@outlook.com>
2020-01-14 11:10:15 +0800

    add comment

--- example/example.cpp
+++ example/example.cpp
@@ -1,14 +1,17 @@
+// comment this #define will not output any log
 #define _xf_log_console
 
 #include "../include/xf_log_console.h"
 
 int main()
 {
+    std::cout << "test print log:" <<std::endl;
+
     _xflog("this is a log");
 
     _xflog("My name is %s, my number is %d", "Frank", 79361486);
 
-    // _xflog(); // error
+    // _xflog(); // error, if _xf_log_console is not defined.
 
     return 0;
 }
