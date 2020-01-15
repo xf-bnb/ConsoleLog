a540b0903e50f3b2f060b23c0a8338d528f6e58d
Frank Xiong <Frank_Xiong@outlook.com>
2020-01-15 20:50:19 +0800

    update

--- include/xf_log_console.h
+++ include/xf_log_console.h
@@ -1,3 +1,13 @@
+/*
+A simple console log library.
+version 1.0.0
+https://github.com/xf-bnb/ConsoleLog
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+SPDX - License - Identifier : MIT
+Copyright(c) 2020 Frank Xiong <https://github.com/xf-bnb>.
+*/
+
 #pragma once
 
 #include <iostream>
@@ -8,6 +18,8 @@
 
 namespace xf::log
 {
+    inline const char* version() { return "1.0.0"; }
+
     // 单条日志最大长度
     inline constexpr unsigned int _max_length() { return 0x0400; }
 
@@ -37,9 +49,9 @@ namespace xf::log
 }   // namespace xf::log
 
 #ifdef  _xf_log_console
-#define _xflog(format, ...)  { char _xf_console_log_text_variable[xf::log::_max_length()]{ 0 };                 \
-        std::snprintf(_xf_console_log_text_variable, xf::log::_max_length(), format __VA_OPT__(,) __VA_ARGS__); \
+#define _xfLog(format, ...)  { char _xf_console_log_text_variable[xf::log::_max_length()]{ 0 };     \
+        std::snprintf(_xf_console_log_text_variable, xf::log::_max_length(), format, __VA_ARGS__);  \
         xf::log::_output_to_console(_xf_console_log_text_variable); }
 #else
-#define _xflog(...)
+#define _xfLog(...)
 #endif  // _xf_log_console
